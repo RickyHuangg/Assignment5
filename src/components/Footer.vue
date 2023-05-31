f42277d42c76d99bdfe8c99f3f11949e340da828
Ricky <Rhruan2@gmail.com>
2023-05-30 22:05:46 -0400

    Mostly done

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<script setup></script>
+
+<template>
+  <div class="footer">
+    <p>Questions? Call 1-857-039-732 ( Not a real number)</p>
+    <div class="links">
+      <a href="">FAQ</a>
+      <a href="">Help Center</a>
+      <a href="">Account</a>
+      <a href="">Media Center</a>
+      <a href="">Investor Relations</a>
+      <a href="">Jobs</a>
+      <a href="">Redeem Gift Cards</a>
+      <a href="">Buy Gift Cards</a>
+      <a href="">Ways to Watch</a>
+      <a href="">Terms of Use</a>
+      <a href="">Privacy</a>
+      <a href="">Cookie Preferences</a>
+      <a href="">Corporate Information</a>
+      <a href="">Contact Us</a>
+      <a href="">Speed Test</a>
+      <a href="">Legal Notices</a>
+      <a href="">Only on Not Netflix</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.links {
+  display: grid;
+  grid-template-columns: 200px 200px 200px 200px;
+  grid-template-rows: 30px 30px 30px 30px;
+  font-size: 12px;
+}
+.footer {
+  color: aliceblue;
+  display: grid;
+  justify-content: center;
+
+  background-color: rgb(0, 0, 0);
+  width: 100vw;
+  height: 45vh;
+}
+a {
+  color: rgb(251, 250, 246);
+}
+a:link {
+  text-decoration: none;
+}
+</style>
